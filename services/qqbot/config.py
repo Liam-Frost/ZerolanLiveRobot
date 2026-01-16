72bc0419b7c4988e51d431027c76b01160540afd
AkagawaTsurunaki <akagawatsurunaki@outlook.com>
2026-01-16 22:54:20 +0800

    refactor: Add more desc

--- services/qqbot/config.py
+++ services/qqbot/config.py
@@ -5,11 +5,11 @@ from pydantic import BaseModel, Field
 
 class QQBotServiceConfig(BaseModel):
     enable: bool = Field(True, description="Whether to enable the QQBotBridge.")
-    qq_num: int = Field(-1, description="QQ number.")
+    qq_num: int = Field(-1, description="Bot's QQ number.")
     ws_uri: str = Field("ws://127.0.0.1:3033", description="Uri of websocket connected to NapCat.")
     ws_token: str = Field("", description="Token for this websocket server.")
     groups: List[int] = Field([], description="Chat groups.")
     friends: List[int] = Field([], description="Chat friends.")
-    root: int = Field(-1, description="Root user.")
+    root: int = Field(-1, description="Root user (master).")
 
 
